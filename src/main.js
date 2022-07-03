38bc399e41e18ee72c4fe5b2ea9485f6b5e0f5b5
Hadid Widiantoro <hadidw3@gmail.com>
2022-07-04 01:10:38 +0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/3YS1Qf5xiOydovi7/scene.splinecode');
